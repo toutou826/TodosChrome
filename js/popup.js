6318cf2b741cc8ef21d275c60a71e93f0bd596c4
Qing Liu <37494307+toutou826@users.noreply.github.com>
2019-11-28 00:55:14 -0500

    make-draggable
    Made the todos draggable

--- js/popup.js
+++ js/popup.js
@@ -2,16 +2,25 @@
 $("input").keypress(function(event){
     if(event.which === 13 && $(this).val() != ""){
         console.log($(this).val());
-       $("<li class='todos'><span class='circle'>O </span>" + $(this).val() + "</li>").insertBefore('.addRow');
-       $(this).val("");
+    $("<li class='todos'><span class='circle'>O </span>" + $(this).val() + "</li>").insertBefore('.addRow');
+    $(this).val("");
     }
 });
 
+//Strike text after double click
 $("ul").on("dblclick","li.todos",function(){
     $(this).toggleClass("completed");
 });
 
+//Remove todo after clicking on circle
 $("ul").on("click","span",function(event){
-    $(this).parent().fadeOut(300);
+    $(this).parent().fadeOut("slow");
     $(this).parent().remove();
 });
+
+
+//Drag and sort
+$(function(){
+    $( "ul" ).sortable();
+});
+
