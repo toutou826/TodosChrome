822d5724ba7a9955afd2e17029432a9a94417931
Qing Liu <37494307+toutou826@users.noreply.github.com>
2019-11-28 14:00:17 -0500

    updated-remove
    changed remove to double click
    changed line through color to black

--- js/popup.js
+++ js/popup.js
@@ -2,20 +2,22 @@
 $("input").keypress(function(event){
     if(event.which === 13 && $(this).val() != ""){
         console.log($(this).val());
-    $("<li class='todos'><span class='circle'>O </span>" + $(this).val() + "</li>").insertBefore('.addRow');
+    $("<li class='todos'>" + $(this).val() + "</li>").insertBefore('.addRow');
     $(this).val("");
     }
 });
 
-//Strike text after double click
-$("ul").on("dblclick","li.todos",function(){
+//Strike text after click
+$("ul").on("click","li.todos",function(){
     $(this).toggleClass("completed");
 });
 
+
+
 //Remove todo after clicking on circle
-$("ul").on("click","span",function(event){
-    $(this).parent().fadeOut("slow");
-    $(this).parent().remove();
+$("ul").on("dblclick","li.todos",function(event){
+    $(this).fadeOut("slow");
+    $(this).remove();
 });
 
 
