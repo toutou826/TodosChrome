5d251dc733d38f338f47a39dec68c456e591f31a
Qing Liu <37494307+toutou826@users.noreply.github.com>
2019-11-28 21:20:29 -0500

    sync-storage
    makde sync working by using google storage api

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,37 @@
-//Add todos when enter is pressed
-$("input").keypress(function(event){
-    if(event.which === 13 && $(this).val() != ""){
-        console.log($(this).val());
-    $("<li class='todos'>" + $(this).val() + "</li>").insertBefore('.addRow');
-    $(this).val("");
+// chrome.storage.sync.get("todo", function (retVal) {
+//     console.log(retVal.value);
+// });
+let todoArray = [];
+
+
+chrome.storage.sync.get('todo',function(keys){
+    if(keys.todo != null){
+        todoArray = keys.todo;
+        for(let i = 0; i < todoArray.length; i ++){
+            $("<li class='todos'>" + todoArray[i] + "</li>").insertBefore('.addRow');
+        }
     }
 });
 
-//Strike text after click
-$("ul").on("click","li.todos",function(){
-    $(this).toggleClass("completed");
+
+//Add todos when enter is pressed
+$("input").keypress(function(event){
+    let inputVal = $(this).val();
+    if(event.which === 13 && inputVal != ""){
+        todoArray.push(inputVal);
+        chrome.storage.sync.set({'todo': todoArray},function(){});
+        $("<li class='todos'>" + inputVal + "</li>").insertBefore('.addRow');
+        $(this).val("");
+
+    }
 });
 
 
 
 //Remove todo after double clicking
 $("ul").on("dblclick","li.todos",function(event){
+    todoArray.splice(todoArray.indexOf($(this)),1);
+    chrome.storage.sync.set({'todo': todoArray},function(){});
     $(this).fadeOut("slow");
     $(this).remove();
 });
